7d085459730ea0abf02a641836f18e5337d41b7b
Matthew Deloughry <matt@ledgardjepson.com>
2018-10-03 16:39:32 +0100

    Added C# Example

---
+++ scripts/csharp_helloworld_trickierstinky.cs
@@ -0,0 +1,13 @@
+// LANGUAGE: C#
+// AUTHOR: Matthew Deloughry
+// GITHUB: https://github.com/trickierstinky
+using System;
+
+namespace HelloWorld {
+    class Program {
+      static void Main(string[] args) {
+        Console.WriteLine("Hello World!");
+        Console.ReadKey();
+      }
+    }
+}
