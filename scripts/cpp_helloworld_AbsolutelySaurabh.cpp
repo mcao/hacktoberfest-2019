5d545d55464882ccb358d81237a43594243f68f4
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2018-10-05 15:34:21 +0530

    hello_world_cpp

---
+++ scripts/cpp_helloworld_AbsolutelySaurabh.cpp
@@ -0,0 +1,13 @@
+// LANGUAGE: C++14
+// AUTHOR: Saurabh Singh
+// GITHUB: https://github.com/AbsolutelySaurabh
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+  cout<<"Hello Hacktoberfest!";  
+  return 0;
+}
