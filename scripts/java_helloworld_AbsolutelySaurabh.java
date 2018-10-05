09eae8f423fc8cbd97ce70c3d192100ec1fd278f
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2018-10-05 15:28:13 +0530

    hello_world_code

---
+++ scripts/java_helloworld_AbsolutelySaurabh.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java-8
+// AUTHOR: Saurabh Singh
+// GITHUB: https://github.com/AbsolutelySaurabh
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, HactoberFest!");
+  }
+}
