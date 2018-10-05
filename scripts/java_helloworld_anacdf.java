39a5af0f3f53185bfb0e1e63076efffa4fb30191
Ana Carolina <anadedavid@gmail.com>
2018-10-05 20:11:29 -0300

    new java helloWorld

---
+++ scripts/java_helloworld_anacdf.java
@@ -0,0 +1,10 @@
+// LANGUAGE: Java
+// ENV: Java
+// AUTHOR: Ana Carolina
+// GITHUB: https://github.com/anacdf
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, World");
+    }
+}
