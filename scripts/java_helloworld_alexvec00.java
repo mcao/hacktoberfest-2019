d885af5c44fca4561fa692aefbf513b6c8c889a4
Alexvec00 <38962279+Alexvec00@users.noreply.github.com>
2018-10-06 14:26:08 +0200

    Update scripts/java_helloworld_alexvec00.java

---
+++ scripts/java_helloworld_alexvec00.java
@@ -0,0 +1,10 @@
+public class java_helloworld_alexvec00
+
+// LANGUAGE: Java
+// AUTHOR: Alex Vec
+// GITHUB: https://github.com/alexvec00
+{
+    public static void main (String[]args){
+    System.out.println("Hello World");
+ }
+}
