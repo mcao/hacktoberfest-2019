27da29b5e611e2c6c9d3e31c032376e88b8c2359
jlander <lander.jon9@gmail.com>
2018-10-13 11:16:22 -0400

    Added hello world in Prolog

---
+++ scripts/prolog_helloworld_landerjon97.pl
@@ -0,0 +1,9 @@
+  /*
+  Author: Jon Lander
+  Purpose: To print hello world in prolog.
+  How to: 
+  Install swi-prolog
+  file->consult->this program
+  type "hello." in the window
+  */
+  hello:- write('Hello, World!'), nl.
