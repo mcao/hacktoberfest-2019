5390567486ba878b54c52c01b7f71d4424c7b3ee
Domenick DiBiase <domenickdibiase@gmail.com>
2018-10-15 00:17:56 -0400

    Created Hello World Program

---
+++ HelloWorld_Domenick.java
@@ -0,0 +1,14 @@
+/**
+ * Hello World Program
+ */
+public class HelloWorld_Domenick {
+
+    /*
+     * Main method responsible for printing "Hello World!"
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
