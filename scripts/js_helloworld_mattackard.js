3cf299d8f66b93aa9156e93158830b25bdcc0422
Matthew Ackard <mattackard@gmail.com>
2018-10-18 17:25:41 -0700

    removed duplicate js script file

--- scripts/js_helloworld_mattackard.js
+++ scripts/js_helloworld_mattackard.js
@@ -3,4 +3,15 @@
 // AUTHOR: Matt Ackard
 // GITHUB: https://github.com/mattackard
 
-console.log('Hello, World!');
+let char = [32, 33, 44, 68, 69, 72, 76, 79, 82, 87];	//character codes for all character in HELLO, WORLD!
+
+let order = [5,4,6,6,7,2,0,9,7,8,6,3,1];		//numbers directed to indexes of char, 
+							//sequenced to build the message
+
+let message = [];
+
+order.forEach((num) => {
+	message.push(String.fromCharCode(char[num]));
+});
+
+console.log(message.join(''));
