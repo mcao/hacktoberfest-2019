2f1cc59dc2f045fad441787c8726754d59f4ab49
gregnicotera <42983801+gregnicotera@users.noreply.github.com>
2018-10-24 23:00:27 -0400

    Create java_helloworld_gregnicotera.java

---
+++ scripts/java_helloworld_gregnicotera.java
@@ -0,0 +1,9 @@
+// LANGUAGE: java
+// ENV: java
+// AUTHOR: Greg Nicotera
+// GITHUB: https://github.com/gregnicotera
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
