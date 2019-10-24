9f159a9f70716e43ce44d56e7069239a025f4540
João <coelhotavares1711@gmail.com>
2019-10-24 20:43:29 -0300

    2019 hacktoberfest

---
+++ scripts/CS_HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CS_HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Happy Hacktoberfest2010 for all! xP");
+        }
+    }
+}
