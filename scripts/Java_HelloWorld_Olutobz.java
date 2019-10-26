0210e842ac1d4438c575be82293e702be4946782
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2019-10-26 22:45:59 +0100

    I added an hello world in java to the script folder

---
+++ scripts/Java_HelloWorld_Olutobz.java
@@ -0,0 +1,10 @@
+public class Java_HelloWorld_Olutobz {
+    public static void main(String[] args) {
+        // LANGUAGE: Java
+        // ENV: IntelliJ Idea
+        // AUTHOR: Damola Olutoba Onikoyi
+        // GITHUB: https://github.com/Olutobz
+
+        System.out.println("Hello World!");
+    }
+}
