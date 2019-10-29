8425f6e17af1d65a2f7eb9bebdff70a0d607b9b3
Tristan MacInnis <tristan@macinnis.net>
2019-10-28 23:32:55 -0700

    add description to how soring works

--- sort.sh
+++ sort.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-perl -pe 's/^(?!####)/xxxxx/g' CONTRIBUTORS.md | tr -d '\n' | perl -pe 's/####/\n####/g' | sort | perl -pe 's/xxxxx/\n/g' > CONTRIBUTORS_sorted.md
+
+# 1. add unique (we hope) string to all non-header lines
+# 2. remove all newlines
+# 3. add a newline before our headers
+# 4. sort all the lines
+# 5. replace our unique (we still hope) string with newlines
+
+perl -pe 's/^(?!####)/xxxxx/g' CONTRIBUTORS.md | tr -d '\n' | perl -pe 's/####/\n####/g' | sort | perl -pe 's/xxxxx/\n/g'
 
