682c8adb4f112b23e6f72de7b4cf35ee93322351
9maguilar <54896717+9maguilar@users.noreply.github.com>
2019-10-30 18:30:45 -0400

    Create 9maguilar_HelloWorld.java

---
+++ scripts/9maguilar_HelloWorld.java
@@ -0,0 +1,8 @@
+public class 9maguilar_HelloWorld{
+  //LANGUAGE: Java
+  //AUTHOR: Marisabel Aguilar
+  //GITHUB: https://github.com/9maguilar
+  public static void main(String[args]){
+  System.out.print("Hello World");
+  }
+}
