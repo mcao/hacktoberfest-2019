6ba598d75aec0a350cb0a0da6de33b2495f5c517
Walisson Farias <38569866+alissonfariias@users.noreply.github.com>
2019-10-30 21:37:34 -0300

    Create javascript_helloworld_alissonfariias.js

---
+++ scripts/javascript_helloworld_alissonfariias.js
@@ -0,0 +1,7 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Walisson Farias
+// GITHUB: https://github.com/alissonfariias
+
+console.log('Hello, world!');
+console.log("Thank you for making me participate in this wonderful event!");
